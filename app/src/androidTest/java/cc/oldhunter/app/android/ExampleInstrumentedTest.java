b4f8cea240ae791b56e01b02bf2dd0f86a89e7b8
WangHaiYu <wanghaiyu@meicai.cn>
2018-01-22 19:15:33 +0800

    try test case

--- app/src/androidTest/java/cc/oldhunter/app/android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cc/oldhunter/app/android/ExampleInstrumentedTest.java
@@ -7,7 +7,10 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import utils.LogUtils;
+import utils.UiUtils;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,7 +23,8 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("cc.oldhunter.app.android", appContext.getPackageName());
+        UiUtils.showToast("xxffll");
+        LogUtils.e("xxff>>>");
     }
 }
--- app/src/test/java/cc/oldhunter/app/android/ExampleUnitTest.java
+++ app/src/test/java/cc/oldhunter/app/android/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package cc.oldhunter.app.android;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +10,14 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
+//    @Test
     public void addition_isCorrect() throws Exception {
+        System.out.println("xxxfff");
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void test(){
+        System.out.println(321);
     }
 }

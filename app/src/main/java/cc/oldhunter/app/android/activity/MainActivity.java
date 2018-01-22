7688207063a6a88a1d8862362909cd9426f45f1c
WangHaiYu <wanghaiyu@meicai.cn>
2018-01-22 16:31:30 +0800

    init

---
+++ app/src/main/java/cc/oldhunter/app/android/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package cc.oldhunter.app.android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cc.oldhunter.app.android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
